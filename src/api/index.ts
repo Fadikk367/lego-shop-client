fe5691d433631a98372432960fbc3b44dcb22cd8
Adrian Furman <fadikk367@gmail.com>
2021-12-17 02:00:42 +0100

    Add production backend url to axios instance

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const apiUrl = 'http://localhost:8000/'
+const apiUrl = 'http://localhost:8000/';
+const prodApiUrl = 'https://blooming-atoll-00837.herokuapp.com/';
 
-const axiosInstance = axios.create({baseURL: apiUrl});
+const axiosInstance = axios.create({baseURL: prodApiUrl});
 
 export default axiosInstance;
